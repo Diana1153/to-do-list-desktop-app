44392c4dd2a56889220ac742a57253c5d6ff6667
Saeganf <Saeganafarias@gmail.com>
2025-03-13 15:31:20 -0500

    added a query and text box to display the query or the to do list

--- todolist.py
+++ todolist.py
@@ -11,11 +11,31 @@ cursor = dataConnector.cursor()
 #app window creation
 root = Tk()
 root.title('To Do List')
+
+#centering the window
 width = str(int(root.winfo_screenwidth()/2)-450)
 height = str(int(root.winfo_screenheight()/2)-300)
 root.geometry("900x600+"+width+'+'+height)
 
+#functions
+def query_todolist() :
+    cursor.execute('select * from to_do_list')
+    whattodo = cursor.fetchall()
 
+    show_work = ''
+    for work in whattodo:
+        show_work += str(work) + "\n"
+
+    query_label.insert('1.0', show_work)
+    print("click")
+
+# Label to display the query results
+query_label = Text(root, height=20, width=107)
+query_label.grid(row=0, column=0, padx=20, pady=20)
+
+# Button to trigger the query function
+query_button = Button(root, text="Show To-Do List", command=query_todolist)
+query_button.grid(row=1, column=0, pady=10)
 
 #run it up Blud
 root.mainloop()
